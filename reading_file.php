eaf0aca20700ebf5f97ea8cde1f72e62a4d9a652
Gladwin KAMBUYLE <gkambuyl@e5r1p4.jnb.42.fr>
2016-10-01 17:54:50 +0200

    read

--- reading_file.php
+++ reading_file.php
@@ -21,43 +21,61 @@ if (isset($_POST['sub']))
 				echo $New_line . '<br>';
 			}
 
-							//*- The calculations -*\\
+									//*- Prep -*\\
 		}
 		$array_len = count($New_line);
-		$last_index = $array_len - 1;
-		$counter = 0;
+		$query_index = $array_len - 1;
+		$fact_index = $array_len - 2;
+		$rule_index = $array_len - 2;
 		$New_array = array("A"=>0, "B"=>0, "C"=>0, "D"=>0, "E"=>0, "F"=>0, "G"=>0, 
 		"H"=>0, "I"=>0, "J"=>0, "K"=>0, "L"=>0, "M"=>0, "N"=>0, "O"=>0, "P"=>0, "Q"=>0, 
 		"R"=>0, "S"=>0, "T"=>0, "U"=>0, "V"=>0, "W"=>0, "X"=>0, "Y"=>0, "Z"=>0);
 
-		function array_keys_recursive($New_Array, $MAXDEPTH = 25, $depth = 0, $arrayKeys = array())
+		$opp_1st = ('(');
+		$opp_2nd = array('|', '^');
+		$opp_3rd = array('+', '!');
+		$opp_res = array("=>", "<=>");
+		$rc = 0;
+		$Rules = array_slice($New_line, $rc, $rule_index);
+		function get_string_between($string, $start, $end)
 		{
-			if($depth < $MAXDEPTH)
-			{
-				$depth++;
-				$keys = array_keys($myArray);
-				foreach($keys as $key)
-				{
-					// if(is_array($myArray[$key]))
-					// {
-						$arrayKeys[$key] = array_keys_recursive($myArray[$key], $MAXDEPTH, $depth);
-					}
-				}
-			}
-
-			return $arrayKeys;
+    		$string = ' ' . $string;
+   			$ini = strpos($string, $start);
+    		if ($ini == 0) return '';
+    		$ini += strlen($start);
+    		$len = strpos($string, $end, $ini) - $ini;
+    		return substr($string, $ini, $len);
 		}
+							//*- The calculations -*\\
 
+							
 		// if ($New_line[$last_index])
 		// {
 			
-		//     foreach($New_line, as $rules)
+		//     for ($counter = 0; $counter <= $array_len - 3; counter++)
 		//     {
-					//if (ctype_upper(rules))
+					//while ($New_line[counter] <= )
+					//{
+						//foreach ($Rules as $rule)
+						//{
+							//$rule = trim.erge_replace("[\s\t]", "", $rule)
+							//for ($i = 0; $i <= (count($rule) - 1); $i++)
+								//{
+									//if ($rule[$cn] == $opp_1st)
+									//{
+										//$bod = get_string_between($rule, '(', ')');
+										//
+									//}
+								//}
+								//rule++;
+						//}
+					//}
+
+					//if (ctype_upper($query_index) && ctype_upper($fact_index))
 					//{
 						//*- Definations -*\\
 
-
+							//
 					//}
 		//     }
 
