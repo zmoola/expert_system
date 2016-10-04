1237fc45bd89f8d36864206e60242a59946036ee
Zubair MOOLA <zmoola@e5r1p5.jnb.42.fr>
2016-10-04 17:31:48 +0200

    ok

--- processing.php
+++ processing.php
@@ -10,7 +10,6 @@ function process_vars($vars, $query, $rules)
     $res = array();
     echo "<br /><br /> Processing<br />";
     for ($cn = 0; $cn < strlen($query); $cn++)
-    {
         if (ctype_alpha($query[$cn]) && ctype_upper($query[$cn]))
             {
                 if ($vars[$query[$cn]] === 1)
@@ -22,7 +21,6 @@ function process_vars($vars, $query, $rules)
                     $res[$query[$cn]] = rev_calc($rules, $query[$cn]);
                 }
             }
-    }
     print_r($res);
 }
 
