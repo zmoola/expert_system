d67158806d45772dc37823750666ef6d66718976
Gladwin KAMBUYLE <gkambuyl@e5r1p4.jnb.42.fr>
2016-10-04 15:49:56 +0200

    ES

--- reading_file.php
+++ reading_file.php
@@ -11,22 +11,29 @@ if (isset($_POST['sub']))
 		$file = $name;
 		$document = file_get_contents($file);
 		$lines = explode("\n", $document);
-		foreach ($lines as $New_line)
+		$lines = array_filter($lines);
+		$lines = implode("\n", $lines);
+		$lines = explode("\n", $lines);
+		$pushed_lines = array();
+		foreach ($lines as $New_lines)
 		{
-			$New_line = trim($New_line);
+			$New_line = trim($New_lines);
 			if($New_line[0] != '#')
 			{
-				$New_line = trim($New_line);
 				$New_line = preg_replace('/#.*/', '', $New_line);
-				echo $New_line . '<br>';
+				if($New_line[0] === '=')
+				$facts = $New_line;
+				else if ($New_line[0] === '?')
+				$query = $New_line;
+				else
+				{
+				$New_line = trim($New_line);
+				array_push($pushed_lines, $New_line);
+				}
 			}
 
 									//*- Prep -*\\
 		}
-		$array_len = count($New_line);
-		$query_index = $array_len - 1;
-		$fact_index = $array_len - 2;
-		$rule_index = $array_len - 2;
 		$New_array = array("A"=>0, "B"=>0, "C"=>0, "D"=>0, "E"=>0, "F"=>0, "G"=>0, 
 		"H"=>0, "I"=>0, "J"=>0, "K"=>0, "L"=>0, "M"=>0, "N"=>0, "O"=>0, "P"=>0, "Q"=>0, 
 		"R"=>0, "S"=>0, "T"=>0, "U"=>0, "V"=>0, "W"=>0, "X"=>0, "Y"=>0, "Z"=>0);
@@ -35,8 +42,6 @@ if (isset($_POST['sub']))
 		$opp_2nd = array('|', '^');
 		$opp_3rd = array('+', '!');
 		$opp_res = array("=>", "<=>");
-		$rc = 0;
-		$Rules = array_slice($New_line, $rc, $rule_index);
 		function get_string_between($string, $start, $end)
 		{
     		$string = ' ' . $string;
@@ -46,30 +51,53 @@ if (isset($_POST['sub']))
     		$len = strpos($string, $end, $ini) - $ini;
     		return substr($string, $ini, $len);
 		}
-							//*- The calculations -*\\
 
-							
-		// if ($New_line[$last_index])
-		// {
-			
-		//     for ($counter = 0; $counter <= $array_len - 3; counter++)
-		//     {
-					//while ($New_line[counter] <= )
-					//{
-						//foreach ($Rules as $rule)
-						//{
-							//$rule = trim.erge_replace("[\s\t]", "", $rule)
-							//for ($i = 0; $i <= (count($rule) - 1); $i++)
-								//{
-									//if ($rule[$cn] == $opp_1st)
-									//{
-										//$bod = get_string_between($rule, '(', ')');
-										//
-									//}
-								//}
+		print_r($pushed_lines);
+		echo "<BR>". $facts."<BR>".$query."<BR>";
+		print_r($New_array);
+							//*- The calculations -*\\
+		$cn = 1;
+		echo "<BR>". $query[cn]. '<BR>';
+		//$pushed_merge = array($New_array, $facts);
+		foreach ($facts as $i)
+		{
+			echo "<BR>". $i."<BR>";
+			if (ctype_alpha($i) && ctype_upper($i))
+			{
+				$New_array[$i] = 1;
+			}
+		}
+		print_r($New_array);
+		/* while($query[cn])
+		 {
+			if (ctype_alpha($query) && ctype_upper($query)) // checking - array \\
+			 {
+				$array_len = count($pushed_lines);
+			    for ($counter = 0; $counter <= $array_len - 3; $counter++)
+			    {
+		 			while ($pushed_lines[counter] <= ($array_len - 3))
+		 			{
+		 				foreach ($pushed_lines as $rule)
+		 				{
+		 					$rule = trim.erge_replace("[\s\t]", "", $rule);
+		 					for ($i = 0; $i <= (count($rule) - 1); $i++)
+		 						{
+		 							if ($rule[$cn] == $opp_1st)
+		 							{
+		 								$bod = get_string_between($rule, '(', ')');
+										 $count = 0;
+		 								while ($bod[$count])
+		 								{
+		 									if ($bod = $opp_2nd)
+											{
+												
+											}
+										}
+									}
+								}
 								//rule++;
-						//}
-					//}
+						}
+					}
 
 					//if (ctype_upper($query_index) && ctype_upper($fact_index))
 					//{
@@ -77,12 +105,11 @@ if (isset($_POST['sub']))
 
 							//
 					//}
-		//     }
-
-		// }
+		   }
+		 }
+	 }*/
 	}
 }
-	 
 
 
 						//*- after program is done --- delete textfile -*\\
